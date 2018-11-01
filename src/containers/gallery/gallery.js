2d955d5f724adfce6cb171b06a34d76bf6b18cb9
ams0696 <aisha_m_stitt@homedepot.com>
2018-11-01 16:12:51 -0400

    router

---
+++ src/containers/gallery/gallery.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Gallery extends Component {
+    render(){
+        return (<h1>Gallery Page</h1>);
+    }
+}
+
+export default Gallery
---
+++ src/containers/home/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import {Router, Route} from 'react-router';
+
+class Home extends Component {
+    render(){
+        return (<h1>Hi</h1>);
+    }
+}
+
+export default Home;
+
