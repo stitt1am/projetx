a23a30929a6de89b76697999ab12717122ec650d
ams0696 <aisha_m_stitt@homedepot.com>
2019-09-20 16:18:12 -0400

    added round navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,13 +5,13 @@ import {Motion, StaggeredMotion, spring} from 'react-motion';
 
 import PropTypes from 'prop-types';
 
-// const { noop } = _;
-const config = { stiffness: 140, damping: 14 };
-const toCSS = (scale) => ({ transform: `scale3d(${scale}, ${scale}, ${scale})` });
-// const toCSS = (rotateX) => ({ transform: `rotateX: ${rotateX}deg` });
 
+const iconArrayOne = [1, 2, 3];
+const iconArrayTwo = [1, 2, 3].reverse();
 const tooltipArrayOne = ['One', 'Two', 'Three'];
-const Button = (props) => <button className={cx('button', props.className)} style={props.style} onClick={props.onClick}>{props.children}</button>;
+const tooltipArrayTwo = ['One', 'Two', 'Three'].reverse();
+
+const Button = (props) => <button className="button" style={props.style} onClick={props.onClick }>{props.children}</button>;
 const ButtonGroup = (props) => <div className="button-group" style={props.style}>{props.children}</div>;
 const Tooltip = (props) => <span className="tooltip">{props.text}</span>;
 
@@ -24,13 +24,10 @@ class Navigation extends Component {
             active: false,
         }
 
-        this.onClick = this.onClick.bind(this);
+        this._onClick = this._onClick.bind(this);
     }
 
-
-
-
-    onClick() {
+    _onClick() {
         this.setState({
             active: !this.state.active
         });
@@ -39,53 +36,95 @@ class Navigation extends Component {
     render() {
         return (
             <div className="container">
-                {/*<Motion*/}
-                {/*defaultStyle={{ scale: 0 }}*/}
-                {/*style={{ scale: spring(1, config) }}>*/}
-                {/*{*/}
-
-                {/*}*/}
-                {/*</Motion>*/}
-                {/*<Motion*/}
-                    {/*defaultStyle={{ scale: 0.675 }}*/}
-                    {/*style={{ scale: spring(0.675, config) }}*/}
-                {/*>*/}
-                    {/*{*/}
-
-                        {/*(value) =>*/}
-                            {/*<Button*/}
-                                {/*onClick={this.onClick}*/}
-                                {/*style={{*/}
-                                    {/*transform: 'scale(' + interpolatingStyles.s + ')',*/}
-                                {/*}}*/}
-
-                            {/*>*/}
-                                {/*<span className={this.state.active ? 'icon active' : 'icon'} />*/}
-
-                                {/*/!*<Tooltip text={tooltipArrayOne[0]}/>*!/*/}
-                            {/*</Button>*/}
-
-                        {/*// <div className="box" style={toCSS(value.scale)} />*/}
-                    {/*}*/}
-                {/*</Motion>*/}
                 <ButtonGroup>
+                    <StaggeredMotion
+                        defaultStyles={[
+                            { x: -45, o: 0 },
+                            { x: -45, o: 0 },
+                            { x: -45, o: 0 },
+                        ]}
+                        styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
+                            return i === prevInterpolatedStyles.length - 1
+                                ? {
+                                    x: spring(this.state.active ? 0 : -45, { stiffness: 330, damping: 20 }),
+                                    o: spring(this.state.active ? 1 : 0, { stiffness: 330, damping: 20 }),
+                                }
+                                : {
+                                    x: spring(prevInterpolatedStyles[i + 1].x, { stiffness: 330, damping: 20 }),
+                                    o: spring(prevInterpolatedStyles[i + 1].o, { stiffness: 330, damping: 20 }),
+                                };
+                        })}>
+                        {interpolatingStyles =>
+                            <ButtonGroup>
+                                {interpolatingStyles.map((style, i) =>
+                                    <Button
+                                        key={i}
+                                        style={{
+                                            position: 'relative',
+                                            right: style.x,
+                                            opacity: style.o,
+                                            pointerEvents: this.state.active ? 'auto' : 'none',
+                                        }}
+                                    >
+                                        <Tooltip text={tooltipArrayOne[i]} />
+                                        {iconArrayOne[i]}
+                                    </Button>
+                                )}
+                            </ButtonGroup>
+                        }
+                    </StaggeredMotion>
+                    <Motion
+                        defaultStyle={{ s: 0.675 }}
+                        style={{ s: spring(this.state.active ? 1 : 0.675, { stiffness: 330, damping: 14 }) }}>
+                        {interpolatingStyles =>
+                            <Button
+                                className="button--large"
+                                onClick={this._onClick}
+                                style={{
+                                    transform: 'scale(' + interpolatingStyles.s + ')',
+                                }}>
+                                <span className={this.state.active ? 'icon active' : 'icon'} />
+                            </Button>
+                        }
+                    </Motion>
 
-            </ButtonGroup>
-
-                <Motion
-                    defaultStyle={{rotateX: 220}}
-                    style={{rotateX: spring(0, {stiffness: 330, damping: 14})}}>
-                    {
-                        (value) => <div className="box" style={toCSS(value.rotateX)}/>
-                    }
-                </Motion>
-                <Motion
-                    defaultStyle={{rotateX: 220}}
-                    style={{rotateX: spring(0, {stiffness: 330, damping: 14})}}>
-                    {
-                        (value) => <div className="box" style={toCSS(value.rotateX)}/>
-                    }
-                </Motion>
+                    <StaggeredMotion
+                        defaultStyles={[
+                            { x: -45, o: 0 },
+                            { x: -45, o: 0 },
+                            { x: -45, o: 0 },
+                        ]}
+                        styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
+                            return i === 0
+                                ? {
+                                    x: spring(this.state.active ? 0 : -45, { stiffness: 330, damping: 20 }),
+                                    o: spring(this.state.active ? 1 : 0, { stiffness: 330, damping: 20 }),
+                                } : {
+                                    x: spring(prevInterpolatedStyles[i - 1].x, { stiffness: 330, damping: 20 }),
+                                    o: spring(prevInterpolatedStyles[i - 1].o, { stiffness: 330, damping: 20 }),
+                                };
+                        })}
+                    >
+                        {interpolatingStyles =>
+                            <ButtonGroup>
+                                {interpolatingStyles.map((style, i) =>
+                                    <Button
+                                        key={i}
+                                        style={{
+                                            position: 'relative',
+                                            left: style.x,
+                                            opacity: style.o,
+                                            pointerEvents: this.state.active ? 'auto' : 'none',
+                                        }}
+                                    >
+                                        <Tooltip text={tooltipArrayTwo[i]} />
+                                        {iconArrayTwo[i]}
+                                    </Button>
+                                )}
+                            </ButtonGroup>
+                        }
+                    </StaggeredMotion>
+                </ButtonGroup>
             </div>
         );
     }
